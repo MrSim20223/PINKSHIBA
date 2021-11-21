ceb503a2103acc6e5156c0bc313eceeb2c501e2b
Salim Chamrouk <70511645+salimchamrouk@users.noreply.github.com>
2021-11-21 04:20:49 +0100

    initial commit

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -2,7 +2,7 @@
 
     $to = "pinkshibatoken@gmail.com";
     $from = $_REQUEST['email'];
-    $name = $_REQUEST['PINKSHIBA'];
+    $name = $_REQUEST['Name'];
     $headers = "From: $from";
     $subject = "You have a message from PinkShiba Website";
 
@@ -16,6 +16,14 @@
 
     $body = "Here is the message you got:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
-    $send = mail($to, $subject, $body, $headers);
+   if( mail($to, $subject, $body, $headers)s)!==true)
+   {
+        die('Fail to send');
+
+
+    }
+    die('Success');
+
+    }
 
 ?>
